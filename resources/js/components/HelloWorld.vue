9a858d131ef8a12f70b44fe99b5b9d2fee5d3b2b
Iraklis Georgas <igeorgas@culture.gr>
2022-07-14 16:14:01 +0300

    Use typed-based defineEmit macro to emit an event

--- resources/js/components/HelloWorld.vue
+++ resources/js/components/HelloWorld.vue
@@ -1,7 +1,12 @@
 <template>
-    <h1>Hello World!</h1>
+    <div>
+        <h1>Hello World!</h1>
+        <button @click="$emit('clicked')"></button>
+    </div>
 </template>
 
 <script setup lang="ts">
-
+const emit = defineEmits<{
+    (e: 'clicked'): void;
+}>();
 </script>
